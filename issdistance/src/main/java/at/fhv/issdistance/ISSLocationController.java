5ba32a5997209bf508eb27d67738f56b0125b354
fbe7236 <amilo303>
2019-01-23 14:33:12 +0100

    neeew

--- issdistance/src/main/java/at/fhv/issdistance/ISSLocationController.java
+++ issdistance/src/main/java/at/fhv/issdistance/ISSLocationController.java
@@ -3,6 +3,7 @@ package at.fhv.issdistance;
 import at.fhv.issdistance.models.DiscoveredResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 public class ISSLocationController {
 
     @RequestMapping(value="/location", method= RequestMethod.GET)
+    @ResponseBody
     public DiscoveredResult getResult() {
 
         RestTemplate restTemplate = new RestTemplate();
